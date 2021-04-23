7b2c6a4ccd4f9fbac3b7c9687e16c0040b6d1548
Vertigo18 <david_rivera@usmp.pe>
2021-04-23 02:37:52 -0500

    Agregar "Contactanos"

---
+++ AppPasteleria/Controllers/ContactanosController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AppPasteleria.Models;
+
+namespace AppPasteleria.Controllers
+{
+    public class ContactanosController : Controller
+    {
+
+        public IActionResult Contactanos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
