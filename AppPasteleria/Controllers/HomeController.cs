152e74a3f7e8477a8685092f8dfa83f2f9685f14
Vertigo18 <david_rivera@usmp.pe>
2021-04-23 01:59:54 -0500

    Agregar Pestaña Contactanos

--- AppPasteleria/Controllers/HomeController.cs
+++ AppPasteleria/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace AppPasteleria.Controllers
             return View();
         }
 
+        public IActionResult Contactanos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
