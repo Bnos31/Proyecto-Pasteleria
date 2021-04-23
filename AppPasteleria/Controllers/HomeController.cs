20f691a6a349cc68073fcd2e5f445ed37bf55e6b
Bryan Ojeda <limpsket.bra@gmail.com>
2021-04-23 03:39:33 -0500

    Productos1

--- AppPasteleria/Controllers/HomeController.cs
+++ AppPasteleria/Controllers/HomeController.cs
@@ -27,6 +27,7 @@ namespace AppPasteleria.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
